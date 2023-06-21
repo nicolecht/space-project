f0d7350adddcd56bac563ec7de155a074c096a94
nicolecht <nicolecht@hotmail.com>
2023-06-21 14:05:41 +0800

    created http server using nodejs to calculate PI

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # space-project
+
+
+# Calculate Pi
+# Using the Gregory-Leibniz Series Formula
+# https://www.wikihow.com/Calculate-Pi#:~:text=Use%20the%20formula.,result%20should%20be%20roughly%203.14.
---
+++ backend/server.js
@@ -0,0 +1,58 @@
+const http = require("node:http");
+
+let mostAccuratePi = 0;
+
+const server = http.createServer((req, res) => {
+  if (req.url.startsWith("/pi") && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ pi: mostAccuratePi }));
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found");
+  }
+});
+
+async function calculatePi() {
+  let previousPi = 0;
+  let currentPi = 0;
+  let sign = 1;
+  let decimalPrecision = 0;
+  let i = 0;
+
+  while (true) {
+    const denominator = 2 * i + 1;
+    const term = sign * (4 / denominator);
+    currentPi += term;
+    sign *= -1;
+
+    const previousPiDecimal = getNthDecimal(previousPi, decimalPrecision);
+    const currentPiDecimal = getNthDecimal(currentPi, decimalPrecision);
+
+    if (previousPiDecimal === currentPiDecimal) {
+      mostAccuratePi = currentPiDecimal;
+      decimalPrecision++;
+      console.log(currentPiDecimal);
+    }
+
+    previousPi += term;
+    i++;
+
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  }
+}
+
+function getNthDecimal(number, decimalPrecision) {
+  if (decimalPrecision === 0) {
+    return Math.floor(number);
+  }
+
+  const factor = 10 ** decimalPrecision;
+  const roundedNumber = Math.floor(number * factor);
+  return roundedNumber / factor;
+}
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  calculatePi();
+});
